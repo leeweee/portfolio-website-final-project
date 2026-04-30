c8de14174f1726b827023a48aff8f66552c8118c
leeweee <daisyloversclub@gmail.com>
2026-04-29 18:36:02 -0700

    Refactor dark mode toggle and add random images

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+// darkmode button
 const themeButton = document.getElementById("themeButton");
 
-themeButton.addEventListener("click", function () {
-  document.body.classList.toggle("dark");
+if (themeButton) {
+  themeButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark");
 
-  if (document.body.classList.contains("dark")) {
-    themeButton.textContent = "Light Mode";
-  } else {
-    themeButton.textContent = "Dark Mode";
+    if (document.body.classList.contains("dark")) {
+      themeButton.textContent = "Light Mode";
+    } else {
+      themeButton.textContent = "Dark Mode";
+    }
+  });
+}
+
+
+// random images for home
+document.addEventListener("DOMContentLoaded", function () {
+  const images = [
+    "numbness.jpg",
+    "untitled(1).jpg",
+    "okaymyles.jpg"
+  ];
+
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
+  const hero = document.querySelector(".hero");
+
+  if (hero) {
+    hero.style.backgroundImage = `url('${randomImage}')`;
+    hero.style.backgroundSize = "cover";
+    hero.style.backgroundPosition = "center";
   }
 });
